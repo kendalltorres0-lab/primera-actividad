90f565264630ef3062087cc9ecb027d29dcf8268
kendalltorres0-lab <kendalltorres0@gmail.com>
2026-04-19 21:49:22 -0600

    tercera fase

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
 let num1=document.getElementById("num1")
 let num2=document.getElementById("num2")
-let num3
+let num3=document.getElementById("num3")
+let num4=document.getElementById("num4")
+let num5
 let button=document.getElementById("button")
 button.addEventListener("click",respuesta)
 function respuesta(){
 
-num3= Number(num1.value) +  Number(num2.value)
+num5= Number(num1.value) +  Number(num2.value) + Number(num3.value) + Number(num4.value) 
 
-alert ("El resultado es"+"   " +num3)
+if (num5>=100){
+
+alert ("El numero es mayor o igual a 100")
+
+
+
+}
+
+else {
+    
+    alert ("el numero es menor a 100")
+}
+
 
 
 
